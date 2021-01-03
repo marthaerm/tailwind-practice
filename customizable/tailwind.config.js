3b8541b1db8bafa21441c3d845e9b7538c7c0fb0
Martha Rodriguez <rdz.marthaelena@gmail.com>
2021-01-03 13:03:15 -0600

    Customized tailwind project created

---
+++ customizable/tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:
+      {
+        primary: {
+          DEFAULT: '#007BFF',
+          700: '#0069D9'
+        },
+        danger: '#DC3545',
+        info: '#18A2B8',
+        success: '#28A745',
+        warning: '#FFC108'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
